8560a3e225f814483ecba2b734e2c618411f5630
Faisal Amin <faisal.amin@bs-23.com>
2021-03-01 22:15:56 +0600

    add splash screen

--- FrQSurvey/FrQSurvey.Android/MainActivity.cs
+++ FrQSurvey/FrQSurvey.Android/MainActivity.cs
@@ -9,7 +9,11 @@ using Android.OS;
 
 namespace FrQSurvey.Droid
 {
-    [Activity(Label = "FrQSurvey", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "FrQSurvey",
+        Icon = "@mipmap/icon",
+        Theme = "@style/MainTheme",
+        MainLauncher = false, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
